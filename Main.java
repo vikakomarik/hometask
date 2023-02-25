3e4e3441a8b4638bc0c90c228e5cf794d3576f61
Viktoriia Komarova <vikamail2011@gmail.com>
2023-02-26 00:31:06 +0100

    initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
+
+\f0\fs26 \cf0 public class Main \{\
+    public static void main(String[] args) \{\
+        System.out.println("Hello world!");\
+    \}\
+\}\
+}
